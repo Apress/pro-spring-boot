072d2bf33011eb334bf4ee1ce59b6d4f42e44e9f
Felipe Gutierrez <felipeg@gmail.com>
2016-01-05 20:26:57 -0700

    Java all in one file

---
+++ ch01-simple-web-app-java-all-cli/WebApp.java
@@ -0,0 +1,22 @@
+package com.apress.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class WebApp {
+
+
+   @RequestMapping("/")
+   public String greetings(){
+      return "<h1> Spring Boot Rocks in Java too!</h1>";
+   }
+
+   public static void main(String[] args) {
+      SpringApplication.run(WebApp.class, args);
+   }
+
+}
