44cb48137608e35e423a637c30e7fb434df1813c
Felipe Gutierrez <felipeg@gmail.com>
2016-02-02 23:13:59 -0700

    updated

--- ch07-simple-mongo-app/src/main/java/com/apress/spring/repository/JournalRepository.java
+++ ch07-simple-mongo-app/src/main/java/com/apress/spring/repository/JournalRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.apress.spring.domain.Journal;
 
 
-public interface JournalRepository extends MongoRepository<Journal, Long> { 
+public interface JournalRepository extends MongoRepository<Journal, String> { 
 	
-		public List<Journal> findByTitleLike(String title);
+		public List<Journal> findByTitleLike(String word);
 }
