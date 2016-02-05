a24ea8c0fe6ae33fc71813f2b1059e8fb787d523
Felipe Gutierrez <felipeg@gmail.com>
2016-02-04 20:30:32 -0700

    updated the package name for the JournalController

--- ch08-spring-boot-journal/src/main/java/com/apress/spring/web/JournalController.java
+++ ch08-spring-boot-journal/src/main/java/com/apress/spring/web/JournalController.java
@@ -1,4 +1,4 @@
-package com.apress.spring.controller;
+package com.apress.spring.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
