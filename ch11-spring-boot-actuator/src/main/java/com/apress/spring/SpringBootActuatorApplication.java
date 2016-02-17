062cb76482156fd62f9c22e868ff56cf8efa3bc5
Felipe Gutierrez <felipeg@gmail.com>
2016-02-17 08:29:35 -0700

    added a non-web actuator

---
+++ ch11-spring-boot-actuator/src/main/java/com/apress/spring/SpringBootActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.apress.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorApplication.class, args);
+	}
+}
