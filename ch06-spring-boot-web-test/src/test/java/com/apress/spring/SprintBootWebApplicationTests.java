97af43e9f8886c025423d2e4391d8826ff4f0c21
Felipe Gutierrez <felipeg@gmail.com>
2016-02-29 08:07:59 -0700

    updated to web

--- ch06-spring-boot-web-test/src/test/java/com/apress/spring/SprintBootWebApplicationTests.java
+++ ch06-spring-boot-web-test/src/test/java/com/apress/spring/SprintBootWebApplicationTests.java
@@ -2,11 +2,13 @@ package com.apress.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SprintBootWebApplication.class)
+@WebAppConfiguration
 public class SprintBootWebApplicationTests {
 
 	@Test
