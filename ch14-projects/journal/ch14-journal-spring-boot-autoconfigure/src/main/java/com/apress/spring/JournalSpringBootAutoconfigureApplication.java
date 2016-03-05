5ea16dad406748e44c20832c456f8015beb3721e
Felipe Gutierrez <fgutierrezcruz@pivotal.io>
2016-03-04 21:09:22 -0600

    updated

--- ch14-projects/journal/ch14-journal-spring-boot-autoconfigure/src/main/java/com/apress/spring/JournalSpringBootAutoconfigureApplication.java
+++ ch14-projects/journal/ch14-journal-spring-boot-autoconfigure/src/main/java/com/apress/spring/JournalSpringBootAutoconfigureApplication.java
@@ -2,10 +2,8 @@ package com.apress.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
-@SpringBootApplication(exclude={JpaRepositoriesAutoConfiguration.class,RepositoryRestMvcAutoConfiguration.class})
+@SpringBootApplication
 public class JournalSpringBootAutoconfigureApplication {
 
 	public static void main(String[] args) {
