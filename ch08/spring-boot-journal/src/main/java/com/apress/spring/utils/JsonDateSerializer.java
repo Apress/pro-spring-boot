dd8f905c4c064be148dfcc25e7e88259b1fbda55
Felipe Gutierrez <felipeg@gmail.com>
2016-03-08 08:21:51 -0700

    updated

--- ch08/spring-boot-journal/src/main/java/com/apress/spring/utils/JsonDateSerializer.java
+++ ch08/spring-boot-journal/src/main/java/com/apress/spring/utils/JsonDateSerializer.java
@@ -4,14 +4,11 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
-@Component
 public class JsonDateSerializer extends JsonSerializer<Date>{
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
